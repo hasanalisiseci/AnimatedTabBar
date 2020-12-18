2050d69a378577d1f29307fba56cc2e2d78d15db
Hasan Ali Siseci <h.alisiseci@gmail.com>
2020-12-18 21:48:55 +0300

    createded tab bar view

---
+++ AnimatedTabBar/AnimatedTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedTabBarApp.swift
+//  AnimatedTabBar
+//
+//  Created by Hasan Ali Şişeci on 18.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
